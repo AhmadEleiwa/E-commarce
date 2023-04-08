d9f9622f33009d7e9064eba032937384d793e697
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-08 23:36:14 +0300

    Adding theming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
-import {red} from '@mui/material/colors'
+
 const theme = createTheme({
   palette: {
-
     primary: {
-      main:"#f458ff"
+      main: "#DB4444",
+      contrastText: '#fafafa'
     },
-    action:{
-      active:'#f1f1f1'
-    }
-
+    secondary: {
+      main: '#00FF66',
+      contrastText: '#fafafa'
+    },
   },
-
-  
 });
 
 
@@ -25,7 +23,8 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <Button color='primary' variant='contained'> click</Button>
+        <Button color='primary' variant='contained' disableElevation> click</Button>
+        <Typography>ss</Typography>
       </ThemeProvider>
     </div>
   );
