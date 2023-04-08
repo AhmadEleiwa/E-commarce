aed346cb91e651c9ae4efbe0e001b1425e25304e
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-08 23:53:53 +0300

    Allow the Discount component take children + has color props

--- src/components/DiscountPercent/index.js
+++ src/components/DiscountPercent/index.js
@@ -1,8 +1,14 @@
 import { Paper } from "@mui/material"
 
-const DiscountPercent =props =>{
-    return <Paper sx={{width:'4em', padding:' 0.2em', backgroundColor:'primary.main', color:'primary.contrastText'}} elevation={0}  >
-        44
+const DiscountPercent = props => {
+    return <Paper sx={{
+        width: '4em',
+        padding: ' 0.2em',
+        backgroundColor: props.color ? props.color+'.main':'primary.main',
+        color: props.color ? props.color+'.contrastText':'primary.contrastText',
+    }}
+        elevation={0}  >
+        {props.children}
     </Paper>
 }
 
