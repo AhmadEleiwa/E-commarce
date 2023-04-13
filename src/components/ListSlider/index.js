1f2ebb6c8a99d5374555e44f4f518aace1d86e51
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-13 18:46:05 +0300

    working on props

--- src/components/ListSlider/index.js
+++ src/components/ListSlider/index.js
@@ -1,13 +1,11 @@
 
-import AbcIcon from "@mui/icons-material/Abc";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 
-import { Button, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import { useRef } from "react";
 import { useState } from "react";
-import CategoryCard from "../Category";
 
 import SubTitle from '../../components/SubTitle'
 import Title from '../../components/Title'
@@ -37,8 +35,8 @@ const ListSlider = props => {
     return <div className="slider" style={{ width: props.width ? props.width : '80%' }}>
         <div className="slider-header">
             <div className="slider-header-titles">
-                <SubTitle >Caasd</SubTitle>
-                <Title>Title</Title>
+                <SubTitle >{props.subTitle}</SubTitle>
+                <Title>{props.title}</Title>
             </div>
             <div className="slider-header-controls">
                 <IconButton onClick={slideLeftHandler}><ArrowBackIcon /></IconButton>
@@ -47,23 +45,7 @@ const ListSlider = props => {
 
         </div>
         <div className="list-slider" ref={slider} >
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST1'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST2'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST3'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST4'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST5'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST6'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST7'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST8'} />
-
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST9'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST10'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST11'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST12'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST13'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST14'} />
-            <CategoryCard icon={<AbcIcon fontSize="inherit" />} title={'TEST15'} />
-
+            {props.children}
         </div>
     </div>
 }
