d3c712ca5268bed502f6a9da69e8e5c721ce5152
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-16 21:35:26 +0300

    reset the App.js to dev changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Footer from './components/Footer/Footer';
+
+
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Skeleton from './Skeleton';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route index path='/' element={<Skeleton>test</Skeleton>}></Route>
+        <Route  path='/product/:id' element={"productId"}></Route>
+
+        <Route  path='*' element={"ERROR"}></Route>
+
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <ThemeProvider theme={theme}>
+    //     <Button color='primary' variant='contained' disableElevation> click</Button>
+    //     <Typography>ss</Typography>
+    //   </ThemeProvider>
+    // </div>
+
+
   );
 }
-
 export default App;
