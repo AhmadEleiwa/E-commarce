5b968eae7a46343f9a4c3686e37461aa9269c8df
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-16 22:08:56 +0300

    Make maxWidth to the container x-large

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { Container } from "@mui/system"
 import HeroSection from '../components/HeroSection'
 const HomePage = props => {
-    return <Container>
+    return <Container maxWidth={'xl'}>
         <HeroSection />
     </Container>
 }
