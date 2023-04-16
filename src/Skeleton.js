605a00b52ad843ef80acd8a342a6857dc82ca1b7
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-16 20:43:08 +0300

    Themeing in Skeleton

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -1,3 +1,20 @@
+import { Button, Typography } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';   
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#DB4444",
+        contrastText: '#fafafa'
+      },
+      secondary: {
+        main: '#00FF66',
+        contrastText: '#fafafa'
+      },
+    },
+  });
+  
 const Skeleton = props =>{
     return   <div className="App">
       <ThemeProvider theme={theme}>
