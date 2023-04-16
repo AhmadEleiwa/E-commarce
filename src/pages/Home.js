375079968953dd21cab614223513c3f0e0709140
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-16 22:10:57 +0300

    add banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import { Container } from "@mui/system"
 import HeroSection from '../components/HeroSection'
+import Banner from '../components/Banner'
 const HomePage = props => {
     return <Container maxWidth={'xl'}>
         <HeroSection />
+        <Banner />
     </Container>
 }
 
