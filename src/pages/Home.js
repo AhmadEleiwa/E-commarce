52db6ed08e63c2e191f1a9f85a4d47f8c4e82faa
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-16 22:23:42 +0300

    Add title prop to Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Container } from "@mui/system"
 import HeroSection from '../components/HeroSection'
 import Banner from '../components/Banner'
 const HomePage = props => {
-    return <Container maxWidth={'xl'}>
+    return <Container maxWidth={'xl'} >
         <HeroSection />
         <Banner title="Test" />
     </Container>
