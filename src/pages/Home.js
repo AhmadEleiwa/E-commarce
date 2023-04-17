a1b0abb1060feb8ca335d7b3353faf34f70c5c08
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-17 18:23:00 +0300

    Addding home page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,22 @@ import Banner from '../components/Banner'
 import Testimg from './test.png'
 import FlashSaleComponent from "../components/FlashSaleComponent"
 import ExploreProducts from "../components/ExploreProducts"
+import BestSellingProduct from "../components/BestSellingProducts"
+import RelatedItemSection from "../components/RelatedItemSection"
 import { Box } from "@mui/system"
 const HomePage = props => {
     return <Container maxWidth={'xl'}  >
 
-        <HeroSection />
-        <FlashSaleComponent />
-        <ExploreProducts />
-        {/* best sel */}
-        {/* Related */}
-        <Banner title="Test" img={Testimg} />
+        <Box display={'flex'} flexDirection={'column'} gap={'4em'} sx={{margin :'2em'}}>
+            <HeroSection />
+            <FlashSaleComponent />
+            <ExploreProducts />
+            <BestSellingProduct />
+            <RelatedItemSection />
+            <Banner title="Test" img={Testimg} />
 
 
+        </Box>
 
 
 
