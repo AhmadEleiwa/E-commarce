3991f5ecbd40b6801e0a6d3a8d17788d052b97b4
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-17 18:16:29 +0300

    rename the file into index.js

---
+++ src/components/BestSellingProducts/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ProductsList from "../ProductsList/ProductsList";
+import Card from "../cards components/card/card";
+import getRandomProducts from "../ProductsList/getRandomProducts/index";
+const randomProductsArray = getRandomProducts();
+
+const BestSellingSection = () => {
+  return (
+      <ProductsList title="Best Selling Products" subtitle="This Month">
+        {randomProductsArray.map((card, index) => (
+          <Card
+            key={card.id}
+            image={card.image}
+            title={card.title}
+            price={card.price}
+            rating={card.rating}
+            id={card.id}
+            showDiscount={false}
+            showOldPrice={true}
+          />
+        ))}
+      </ProductsList>
+  );
+};
+
+export default BestSellingSection;
