ec41bd60ff584bf0c97e6493d7559bfd9681b06f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-19 00:03:20 +0300

    responsive banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,14 +4,14 @@ import './style.css'
 const Banner = props => {
     return <div className="banner">
         <div className="body">
-            <Typography color='secondary'>
+            <Typography color='secondary' variant="h3" fontSize={'inherit'}>
                 Categories
             </Typography>
-            <Typography variant="h2">
+            <Typography variant="h2" fontSize={'inherit'}>
                 {props.title}
             </Typography>
             <TimersItems timeAsList={['23', '05', '59', '35']} />
-            <Button color={'secondary'} variant='contained' disableElevation sx={{ padding: '1em 3em ' }}>Buy Now</Button>
+            <Button color={'secondary'}  variant='contained' disableElevation className="btn" size='inherit' sx={{ padding: '1em 3em ' }}>Buy Now</Button>
         </div>
         <div className="img-cover">
             <img src={props.img} alt={props.title} />      
