da6e74aa56e8c9510a435a4c927a58de3a6ea0bb
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-19 00:34:18 +0300

    DateTimer responsive

--- src/components/CountDownComponents/DateTimeDisplay/DateTimeDisplay.js
+++ src/components/CountDownComponents/DateTimeDisplay/DateTimeDisplay.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Typography, Box } from "@mui/material";
-
+import './style.css'
 const DateTimeDisplay = ({ value, type }) => {
   const dotsColor = "#E07575";
   return (
-    <Box ml={2}>
-      <Typography ml={2} fontSize={12} fontWeight={500}>
+    <Box ml={0} className="time-label"  >
+      <Typography className="label" fontSize={'inherit'} fontWeight={500}>
         {type}
       </Typography>
       <Box display="flex">
         {type !== "Days" && (
-          <Typography mr={0} color={dotsColor} fontSize={32}>
+          <Typography color={dotsColor} fontSize={'inherit'}>
             :
           </Typography>
         )}
 
-        <Typography ml={2} fontSize={32} fontWeight={700}>
+        <Typography  fontSize={'inherit'} fontWeight={700}>
           {value}
         </Typography>
       </Box>
