674c1d70370e92fbc46f2f46f47878abd3e78176
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-19 18:44:50 +0300

    responsive product details page

--- src/components/ProductImages/ProductImages.js
+++ src/components/ProductImages/ProductImages.js
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { useState } from 'react';
 import AsideImages from '../AsideImages/AsideImages';
 import MainImage from '../MainImage/MainImage';
@@ -5,12 +6,12 @@ import MainImage from '../MainImage/MainImage';
 export default function ProductImages({arr}){
     const [mainImg,setMainImg]=useState(arr[0]);
     return(
-        <>
+       <Box display={'flex'} flexDirection="row" alignItems={'center'} sx={{width:'50%'}} >
         <AsideImages
                 images={arr}
                 handleOnClick={setMainImg}
                 />
         <MainImage img={mainImg}/>
-        </>
+        </Box>
     );
 }
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,8 +1,10 @@
 import { Container } from "@mui/system"
 import DetailsOfProduct from '../components/DetailsOfProduct/DetailsOfProduct'
+import RelatedItemSection from '../components/RelatedItemSection'
 const ProductDetailsPage = props => {
     return <Container maxWidth={'xl'}  >
         <DetailsOfProduct />
+        <RelatedItemSection />
     </Container>
 }
 
