5ba6f183e128f6a49de657927507e465d21ebff0
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-19 19:10:40 +0300

    fix the related prooduct Main image

--- src/components/ProductImages/ProductImages.js
+++ src/components/ProductImages/ProductImages.js
@@ -3,15 +3,15 @@ import { useState } from 'react';
 import AsideImages from '../AsideImages/AsideImages';
 import MainImage from '../MainImage/MainImage';
 
-export default function ProductImages({arr}){
-    const [mainImg,setMainImg]=useState(arr[0]);
-    return(
-       <Box display={'flex'} flexDirection="row" alignItems={'center'} sx={{width:'50%'}} >
-        <AsideImages
+export default function ProductImages({ arr }) {
+    const [mainImg, setMainImg] = useState(arr[0]);
+    return (
+        <Box display={'flex'} flexDirection="row" alignItems={'center'} sx={{ width: '50%' }} >
+            <AsideImages
                 images={arr}
                 handleOnClick={setMainImg}
-                />
-        <MainImage img={mainImg}/>
+            />
+            <MainImage img={arr[0]} />
         </Box>
     );
 }
