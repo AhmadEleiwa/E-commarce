16e28552c0675217fb4ecb81170faa35e61c5237
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-20 22:01:53 +0300

    put the changes from dev

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-
 import './App.css';
-import Feature from'./Components/Feature/Feature'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Skeleton from './Skeleton';
+
+import HomePage from './pages/Home';
+import ProductDetailsPage from './pages/ProductDetails';
+import ErrorNotFound from './pages/ErrorNotFound';
 
 function App() {
   return (
-   <>
-   <Feature/> 
-  </>
-    
+    <Router>
+      <Routes>
+        <Route index element={<Skeleton> <HomePage /> </Skeleton>}></Route>
+        <Route  path='/product/:id' element={<Skeleton><ProductDetailsPage /></Skeleton>}></Route>
+
+        <Route  path='*' element={<Skeleton><ErrorNotFound /> </Skeleton>}></Route>
+
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <ThemeProvider theme={theme}>
+    //     <Button color='primary' variant='contained' disableElevation> click</Button>
+    //     <Typography>ss</Typography>
+    //   </ThemeProvider>
+    // </div>
+
+
   );
 }
-
 export default App;
